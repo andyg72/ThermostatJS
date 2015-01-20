d1b43ce98b5ce4e19e59b1c54f6dce3e90c7d923
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-20 17:15:18 +0000

    Added powerSavingModeSwitchOff function to pass test.

--- js/thermostat.js
+++ js/thermostat.js
@@ -3,14 +3,14 @@ var Thermostat = function() {
   this.temperature = 20;
   this.temperatureIncrement = 1;
   this.temperatureMinimum = 10;
-  this.temperatureMaximumPSM = 25;
+  this.temperatureMaximum = 25;
   this.powerSavingMode = true;
 
 };
 
 Thermostat.prototype.increaseTemperature = function(changeTempBy) {
-  if (this.temperature + changeTempBy > this.temperatureMaximumPSM) {
-    this.temperature = this.temperatureMaximumPSM
+  if (this.temperature + changeTempBy > this.temperatureMaximum) {
+    this.temperature = this.temperatureMaximum;
   }
   else {
     this.temperature += changeTempBy;
@@ -27,5 +27,6 @@ Thermostat.prototype.decreaseTemperature = function(changeTempBy) {
 };
 
 Thermostat.prototype.powerSavingModeSwitchOff = function() {
+  this.temperatureMaximum = 32;
   this.powerSavingMode = false;
 };
