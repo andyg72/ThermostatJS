a9c9247be68c1b2c5ce50cf059daf3867af22739
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-21 18:35:11 +0000

    Added ability to change temperature using buttons.

--- js/interface.js
+++ js/interface.js
@@ -2,11 +2,21 @@ console.log("hello world");
 
 var thermostat = new Thermostat();
 
+var updateTemperature = function() {
+  $('.temperature').text(thermostat.temperature);
+};
+
 $(document).ready(function() {
   $('.temperature').text(thermostat.temperature);
+
   $('.change-temp-up').on('click', function() {
     thermostat.increaseTemperature();
-    $('.temperature').text(thermostat.temperature);
+    updateTemperature();
+  });
+
+  $('.change-temp-down').on('click', function() {
+    thermostat.decreaseTemperature();
+    updateTemperature();
   });
 
 
