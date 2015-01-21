0a39fadf369f92504f963e406414130094db384b
Andy <andygnewman@gmail.com>
2015-01-21 18:32:19 +0000

    add functionality to temp up button

--- js/interface.js
+++ js/interface.js
@@ -1,1 +1,13 @@
-interface.js
+console.log("hello world");
+
+var thermostat = new Thermostat();
+
+$(document).ready(function() {
+  $('.temperature').text(thermostat.temperature);
+  $('.change-temp-up').on('click', function() {
+    thermostat.increaseTemperature();
+    $('.temperature').text(thermostat.temperature);
+  });
+
+
+});
